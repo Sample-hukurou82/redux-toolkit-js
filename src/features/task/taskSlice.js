82845e27aa31f9b455db46bcd99e7ec49e9e0d96
hukurou82 <hukurou82@gmail.com>
2021-02-16 11:28:45 +0900

    taskSlice

---
+++ src/features/task/taskSlice.js
@@ -0,0 +1,54 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const taskSlice = createSlice({
+  name: "task",
+  initialState: {
+    idCount: 3,
+    tasks: [
+      {
+        id: 1,
+        title: "TASK A",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "TASK B",
+        completed: true,
+      },
+      {
+        id: 3,
+        title: "TASK C",
+        completed: false,
+      },
+    ],
+  },
+  reducers: {
+    newTask: (state, action) => {
+      state.idCount++;
+      const newItem = {
+        id: state.idCount,
+        title: action.payload,
+        completed: false,
+      };
+      state.tasks = [newItem, ...state.tasks];
+    },
+
+    completeTask: (state, action) => {
+      const task = state.tasks.find((t) => t.id === action.payload.id);
+      if (task) {
+        task.completed = !task.completed;
+      }
+    },
+
+    deleteTask: (state, action) => {
+      //選択されたタスク以外のものをtasksに上書きする
+      state.tasks = state.tasks.filter((t) => t.id !== action.payload.id);
+    },
+  },
+});
+
+export const { newTask, completeTask, deleteTask } = taskSlice.actions;
+
+export const selectTasks = (state) => state.task.tasks;
+
+export default taskSlice.reducer;
